7106e0c7c3b9cee54ce11c0545ecb2319b4293c5
Nazareno Carlesso <nazarenocarlessob@gmail.com>
2023-07-11 21:12:45 -0300

    router fix

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,13 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { Navigate, createBrowserRouter } from 'react-router-dom'
 import { CalendarApp } from '../CalendarApp'
 import { LogInPage } from '../auth/pages/LogInPage'
 import { CalendarPage } from '../calendar/pages/CalendarPage'
 
 export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to='/calendar' />
+  },
   {
     path: '/calendar',
     element: <CalendarApp />,
